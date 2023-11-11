5d36a568eed81a5ca3c9caf6edad20daf9237d7e
Stefan Claas <13186616+stefanclaas@users.noreply.github.com>
2023-11-11 18:59:45 +0100

    Add files via upload

--- hsubgen.go
+++ hsubgen.go
@@ -1,31 +1,24 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/sha256"
 	"encoding/hex"
 	"flag"
 	"fmt"
 	"os"
-	"crypto/rand"
-	"crypto/sha256"
 )
 
-const (
-	hsublen int = 48  // New hsubs will be trimmed to this length
+var (
+	hsublen int
 )
 
 type hsub struct {
-	key string
+	key     string
 	subject string
 }
 
-// hsubtest takes a given key and hsub as input.  It generates a new hsub
-// using the key and IV from supplied hsub and test for a collision.
 func (h *hsub) hsubtest() bool {
-	sublen := len(h.subject)
-	if sublen <= 32 || sublen > 96 {
-		fmt.Println("Error: hsub must be between 32 and 96 chars")
-		os.Exit(2)
-	}
 	iv, err := hex.DecodeString(h.subject[:16])
 	if err != nil {
 		panic(err)
@@ -33,14 +26,13 @@ func (h *hsub) hsubtest() bool {
 	digest := sha256.New()
 	digest.Write(iv)
 	digest.Write([]byte(h.key))
-	newhsub := hex.EncodeToString(append(iv, digest.Sum(nil)...))[:sublen]
+	newhsub := hex.EncodeToString(append(iv, digest.Sum(nil)...))[:len(h.subject)]
 	if newhsub == h.subject {
 		return true
 	}
 	return false
 }
 
-// hsubgen creates a new hsub using a supplied key.
 func (h *hsub) hsubgen() string {
 	iv := make([]byte, 8)
 	_, err := rand.Read(iv)
@@ -50,30 +42,40 @@ func (h *hsub) hsubgen() string {
 	digest := sha256.New()
 	digest.Write(iv)
 	digest.Write([]byte(h.key))
-	return hex.EncodeToString(append(iv, digest.Sum(nil)...))[:hsublen]
+	hsub := hex.EncodeToString(append(iv, digest.Sum(nil)...))
+	for len(hsub) < hsublen {
+		digest.Reset()
+		digest.Write([]byte(hsub))
+		hsub += hex.EncodeToString(digest.Sum(nil))
+	}
+	return hsub[:hsublen]
 }
 
 func main() {
+	flag.IntVar(&hsublen, "l", 48, "Length of the hsub")
 	flag.Parse()
 	cmdargs := flag.Args()
 	switch len(cmdargs) {
 	case 1:
-		// One arg provided - Generate a new hsub
+		if hsublen < 32 || hsublen > 96 {
+			fmt.Println("Error: hsub must be between 32 and 96 characters")
+			os.Exit(2)
+		}
 		h := new(hsub)
 		h.key = cmdargs[0]
 		fmt.Println(h.hsubgen())
 	case 2:
-		// Two args provided - Test the hsub against the key
 		h := new(hsub)
 		h.key = cmdargs[0]
 		h.subject = cmdargs[1]
-		if ! h.hsubtest() {
+		if !h.hsubtest() {
 			fmt.Println("Fail: hsub not generated with this key")
 			os.Exit(1)
 		}
 		fmt.Println("Validated: hsub is valid for this key")
 	default:
-		fmt.Println("Usage: hsubtest <key> [subject]")
+		fmt.Println("Usage: hsubgen [-l int] <key> [subject]")
 		os.Exit(2)
 	}
 }
+
